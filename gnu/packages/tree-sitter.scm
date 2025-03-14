7beed52c01544e4f80efc19b43a9d43b1fc0e872
Karl Hallsby <karl@hallsby.com>
2025-03-14 14:46:37 -0500

    gnu: Add tree-sitter-ada.
    * gnu/packages/tree-sitter.scm (tree-sitter-ada): New variable.
    
    Change-Id: I0f7cb2944ac8c42a1e5d66fc2ee734fa0bbf8373

--- gnu/packages/tree-sitter.scm
+++ gnu/packages/tree-sitter.scm
@@ -814,3 +814,18 @@ which will be used as a snippet in origin."
              (("\\(integer_decimal\n") "(integer_decimal)\n")
              (("\\(integer\\)") "")
              (("\"0\")") "\"0\"")))))))
+
+(define-public tree-sitter-ada
+  (let ((version "0.1.0") ; In package.json, but untagged
+        (commit "e8e2515465cc2d7c444498e68bdb9f1d86767f95")
+        (revision "0"))
+    (tree-sitter-grammar
+     "ada" "Ada"
+     "0vwi4yc6hs7k4djkwhk09q19lw1nswjfh3aksh2mzqyi3ni9byx9"
+     (git-version version revision commit)
+     #:repository-url "https://github.com/briot/tree-sitter-ada"
+     #:commit commit
+     ;; cleanup-snippet is part of tree-sitter-grammar's snippet field but the
+     ;; files this default snippet deletes are not present in the source.
+     ;; Use a fake snippet to allow things to happen.
+     #:get-cleanup-snippet (lambda _ #t))))
